fcee7178db9631a713c05f1bef54d8d30439a57c
Salem Gelidi <salemgelidi@gmail.com>
2025-02-14 16:12:34 +0000

    end of ch9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -29,6 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = 'rango:login'
+
+
 
 # Application definition
 
@@ -100,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS':{'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
